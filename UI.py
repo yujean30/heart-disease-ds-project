56700ce7ce8399b8d74c8b9e5275b462b3d3c009
yujean30 <tanyujean66@gmail.com>
2026-09-03 22:39:01 +0800

    Uncomment plotly imports in UI.py for data visualization functionality

--- UI.py
+++ UI.py
@@ -11,8 +11,8 @@ import pandas as pd
 import numpy as np
 import joblib
 from PIL import Image
-#import plotly.express as px
-#import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
 
 # ---------------------------------------------------------
 # 1. Page Configuration
